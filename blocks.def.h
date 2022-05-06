bf743c7beeca423ea0acbf6973c5fc7688318e8d
Agustín Octavio Videla <agustinvidela0198@gmail.com>
2022-05-06 16:47:53 -0300

    getting ready to add brightness plugin

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,16 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+    {"", "music 2> /dev/null", 1, 11},
+
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%Y %b %d (%a) %H:%M'",					5,		0},
+    {"", "volume", 0, 10},
+
+    {"", "battery", 20, 12},
+
+	{"", "date '+%Y %b %d (%a) %H:%M'",					30,		0},
+
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
